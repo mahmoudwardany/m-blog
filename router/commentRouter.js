979b054f15683ff61deb0b27bd6a99f2af05eded
shoroukshorouk <mahmoudwardany18@gmail.com>
2025-01-18 19:38:30 +0200

    chore(comments) 1_refactor comments controller  and route 2_use import instead require

--- router/commentRouter.js
+++ router/commentRouter.js
@@ -1,16 +1,16 @@
-const { createCommentCtrl, getCommentCtrl, deleteCommentCtrl, updateCommentCtrl } = require('../controller/comments/comment')
-const validObjectId = require('../middleware/validObjectId')
-const { verifyToken, verifyTokenAndAdmin } = require('../middleware/verifyToken')
+import express from 'express';
+import { createCommentCtrl, getCommentCtrl, deleteCommentCtrl, updateCommentCtrl } from '../controller/comments/comment';
+import validObjectId from '../middleware/validObjectId';
+import { verifyToken, verifyTokenAndAdmin } from '../middleware/verifyToken';
 
-const router=require('express').Router()
-
+const router = express.Router();
 
 router.route('/')
-.post(verifyToken,createCommentCtrl)
-.get(verifyTokenAndAdmin,getCommentCtrl)
+    .post(verifyToken, createCommentCtrl)
+    .get(verifyTokenAndAdmin, getCommentCtrl);
 
-router.route("/:id")
-.delete(validObjectId,verifyToken,deleteCommentCtrl)
-.put(validObjectId,verifyToken,updateCommentCtrl)
+router.route('/:id')
+    .delete(validObjectId, verifyToken, deleteCommentCtrl)
+    .put(validObjectId, verifyToken, updateCommentCtrl);
 
-module.exports=router
+export default router;
